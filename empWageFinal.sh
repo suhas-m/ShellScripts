817f195bd7636f906d9543ea5d45cc6407f32a46
Suhas Mhatre <suhas.mhatre@bridgelabz.com>
2019-11-09 18:41:18 +0530

    added the missing variable empWageperHour

--- empWageFinal.sh
+++ empWageFinal.sh
@@ -1,14 +1,13 @@
 #!/bin/bash -x
-
+empRatePerHr=20
 isPresent=1;
-
 employeeCheck=$((RANDOM%2));
 
 if [ $isPresent -eq $employeeCheck ];
 
 then 
         empHrs=8;
-elif [ $isPartTime -eq $randomCheck ];
+elif [ $isPartTime -eq $employeeCheck ];
 then
         empHrs=4;
 else 
