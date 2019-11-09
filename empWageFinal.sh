e1511adf0dacc17986d22fc0c5754914015c3654
Suhas Mhatre <suhas.mhatre@bridgelabz.com>
2019-11-09 17:41:24 +0530

    Added part time wage

--- empWageFinal.sh
+++ empWageFinal.sh
@@ -5,9 +5,12 @@ randomCheck=$((RANDOM%2));
 
 if [ $isPresent -eq $randomCheck ];
 then 
-   empRatePerHr=20;
-   empHrs=8;
-   salary=$(($empHrs*$empRatePerHr));
+        empHrs=8;
+elif [ $isPartTime -eq $randomCheck ];
+then
+        empHrs=4;
 else 
-   salary=0;
+   empHrs=0;
 fi
+
+salary=$(($empHrs*$empRatePerHr));
