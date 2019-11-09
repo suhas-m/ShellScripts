b16478fa91f22813d70b1729eda592df1e8ed7f8
suhas-m <44285812+suhas-m@users.noreply.github.com>
2019-11-09 17:16:36 +0530

    Adding daily wage to the program

--- empWageFinal.sh
+++ empWageFinal.sh
@@ -5,7 +5,9 @@ randomCheck=$((RANDOM%2));
 
 if [ $isPresent -eq $randomCheck ];
 then 
-	echo "Employee is Present";
+   empRatePerHr=20;
+   empHrs=8;
+   salary=$(($empHrs*$empRatePerHr));
 else 
-	echo "Employee is Abscent";
+   salary=0;
 fi
